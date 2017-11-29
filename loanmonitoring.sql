7d17f0497c1ac6a8f3ed66b8f89835caf1538717
kath <kath@email.com>
2017-11-29 09:19:53 +0800

    added customers table

--- loanmonitoring.sql
+++ loanmonitoring.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `loanmonitoring` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `loanmonitoring`;
 -- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
 --
 -- Host: localhost    Database: loanmonitoring
@@ -16,6 +14,31 @@ USE `loanmonitoring`;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `customers`
+--
+
+DROP TABLE IF EXISTS `customers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `customers` (
+  `custid` int(11) NOT NULL,
+  `custname` varchar(45) NOT NULL,
+  `address` varchar(45) NOT NULL,
+  PRIMARY KEY (`custid`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `customers`
+--
+
+LOCK TABLES `customers` WRITE;
+/*!40000 ALTER TABLE `customers` DISABLE KEYS */;
+INSERT INTO `customers` VALUES (101,'Clark Kent','SmallVille'),(102,'Bruce Wayne','Gotham City'),(103,'Peter Parker','Forest Hills'),(104,'Tony Stark','Long Island');
+/*!40000 ALTER TABLE `customers` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -26,4 +49,4 @@ USE `loanmonitoring`;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-11-29  8:59:27
+-- Dump completed on 2017-11-29  9:19:34

cb68b4aa755ec9e2ff67d6d83b68c81c064022f2
franchescaderije <chescaderije@gmail.com>
2017-12-03 12:53:34 +0800

    created a java app that contains the code for songs, crud

--- SongsCrud/src/songscrud/SongsCrud.java
+++ SongsCrud/src/songscrud/SongsCrud.java
@@ -13,8 +13,29 @@ public class SongsCrud {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         // TODO code application logic here
+            Class.forName("com.mysqljdbc.Driver");
+            
+            String conStr = "jdbc:mysql://localhost:3306/dbms?user=root&password=";
+            Connection con = DriverManager.getConnection(conStr);
+            System.out.println("Connected");
+            
+            String stSel = "SELECT * FROM songs";
+            
+            Statement stmt = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+            ResultSet rs = stmt.executeQuery(stSel);
+            
+            rs.beforeFirst();
+            while (rs.next()) {
+                int song_id = rs.getInt(1);
+                String title = rs.getString("song title");
+                
+                /*System.out.println("%3d %-25s %10.2f\n", song_id, title);*/
+                
+            }
+            rs.close();
+            con.close();
     }
     
 }

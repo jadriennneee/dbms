89f3a0faf7a58833ce2339b2581d5f9ab4ab3f8d
franchescaderije <chescaderije@gmail.com>
2017-12-03 09:23:56 +0800

    added a new project folder for songs, CRUD

---
+++ SongsCrud/src/songscrud/SongsCrud.java
@@ -0,0 +1,20 @@
+/*
+ * This will serve as a code(crud) for songs
+ */
+package songscrud;
+import java.sql.*;
+
+/**
+ *
+ * @author Franchesca Derije
+ */
+public class SongsCrud {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}

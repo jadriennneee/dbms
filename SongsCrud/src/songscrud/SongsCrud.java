0269a43eec5d9ef051a9e9ebfc8aca855b414663
franchescaderije <chescaderije@gmail.com>
2017-12-03 21:53:36 +0800

    edited the code for sings crud and added a method to get the duration

--- SongsCrud/src/songscrud/SongsCrud.java
+++ SongsCrud/src/songscrud/SongsCrud.java
@@ -31,52 +31,61 @@ public class SongsCrud {
             System.out.println("Choose among the following choices:");
             System.out.println("1: Show Table\n2: Insert new data\n"
                 + "3:Delete data\n4: Update table content");
-            int choice = kbd.nextInt();
+            int choice = user_input.nextInt();
             
+            /*SHOW TABLE*/
             if(choice == 1) {
                 rs.beforeFirst();
-                System.out.printf("%2s %10s %26s", "song id", "title", "album number");
+                System.out.printf("%2s %10s %21s %26s", "song id", "title", "duration", "album number\n");
                 
                 while (rs.next()){
                     int songid = rs.getInt(1);
                     String title = rs.getString("title");
-                    /*char duration = rs.getChar("duration");*/
+                    String duration = rs.getString("duration");
                     int albumno = rs.getInt("album_no");
                     
-                    System.out.printf("%-10d %-20s %10s\n", song_id,
-                        title, album_no);
-                        kbd.close();
+                    System.out.printf("%-10d %-20s %25s %-15s\n", songid,
+                        title, albumno);
+                        user_input.close();
                 }
+              /*INSERT NEW DATA*/
             }else if(choice == 2){
                 System.out.println("Song id: ");
-                kbd.nextInt();
-                int song_id = kbd.nextInt();
+                user_input.nextInt();
+                int song_id = user_input.nextInt();
                 System.out.println("Title: ");
-                String title = kbd.nextLine();
+                String title = user_input.nextLine();
+                System.out.println("Duration: ");
+                String duration = user_input.nextLine();
                 System.out.println("Album no: ");
-                kbd.nextInt();
-                int album_no = kbd.nextInt();
+                user_input.nextInt();
+                int album_no = user_input.nextInt();
                 
                 PreparedStatement psi;
-                String stIns = "INSERT INTO songs(song_id, title, album_no) VALUES(?,?,?)";
+                String stIns = "INSERT INTO songs(song_id, title, duration, album_no) VALUES(?,?,?,?)";
                 psi = con.prepareStatement(stIns);
                 psi.setInt(1, song_id);
                 psi.setString(2, title);
+                psi.setString(3, duration);
                 psi.setInt(4, album_no);
                 psi.executeUpdate();
+                
+            /*DELETE DATA*/
             }else if(choice == 3){
                 rs.last();
                 rs.deleteRow();
+                
+            /*UPDATE DATA*/
             }else if(choice == 4){
                 System.out.println("Column Number: ");
-                int columnNumber = kbd.nextInt();
+                int columnNumber = user_input.nextInt();
                 System.out.println("Row Number: ");
-                int rowNumber = kbd.nextInt();
+                int rowNumber = user_input.nextInt();
                 System.out.println("Original Content: ");
-                kbd.nextLine(); //Bypass Bug
-                String originalContent = kbd.nextLine();
+                user_input.nextLine(); //Bypass Bug
+                String originalContent = user_input.nextLine();
                 System.out.println("New Content: ");
-                String newContent = kbd.nextLine();
+                String newContent = user_input.nextLine();
                 rs.first();
                 rs.updateString(columnNumber, originalContent); //Select the row
                 rs.absolute(rowNumber); //Change the number to the corresponding row

b34276fd078302b1421ad485b77dd6f55cc1b962
jadriennneee <jadriennneee@users.noreply.github.com>
2017-12-09 09:35:11 +0800

    Edited Playlist. Added While loop

--- Playlist/src/playlist/Playlist.java
+++ Playlist/src/playlist/Playlist.java
@@ -22,63 +22,88 @@ public class Playlist {
         
         CallableStatement callsp = null;
 
-        System.out.print("Enter your choice: ");
-        int choice = console.nextInt();
-        
-        switch(choice){
-            case 1:
-                //Add Song
-                System.out.println("Song Name: ");
-                console.nextLine(); //Bypass Bug
-                String songTitle = console.nextLine();
-                System.out.println("Song Duration; ");
-                String songDuration = console.nextLine();
-                //Call for the stored procedure in mySQL
-                String callSong = "{call addSong(?,?)}";
-                callsp = con.prepareCall(callSong);
-                //Set Values for Song
-                callsp.setString(1, songTitle);
-                callsp.setString(2, songDuration);
-                callsp.executeUpdate();
+        //System.out.print("Enter your choice: ");
+        //int choice = console.nextInt();
+        int choice = 0;
+        while(choice != 13){
+            System.out.print("Enter your choice: ");
+            choice = console.nextInt();
+            switch(choice){
+                case 1:
+                    addSong(console, con);
+                    addArtist(console, con);
 
-                //Add Artist
-                System.out.println("Artist's Last Name: ");
-                String artistLastName = console.nextLine();
-                System.out.println("Artist's First Name: ");
-                String artistFirstName = console.nextLine();
-                System.out.println("Screen Name: ");
-                String screenName = console.nextLine();
-                System.out.println("Gender: ");
-                String gender = console.nextLine();
-                //Call for the stored procedure
-                String callArtist = "{call addArtist(?,?,?,?)}";
-                callsp = con.prepareCall(callArtist);
-                //Set Values
-                callsp.setString(1, artistLastName);
-                callsp.setString(2, artistFirstName);
-                callsp.setString(3, screenName);
-                callsp.setString(4, gender);
-                callsp.executeUpdate();
-                
-                System.out.println("Details has been added to the table.");
-                //View results on MySQL 
-                break;
-            case 2:
-            case 3:
-            case 4:
-            case 5:
-            case 6:
-            case 7:
-            case 8:
-            case 9:
-            case 10:
-            case 11:
-            case 12:
-            case 13:
-            default:
-                System.out.println("Please enter a valid choice");
-                break;
+                    System.out.println("Details has been added to the table.");
+                    //View results on MySQL    
+                    break;
+                case 2: 
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                case 8:
+                    break;
+                case 9:
+                    break;
+                case 10:
+                    break;
+                case 11:
+                    break;
+                case 12:
+                    break;
+                case 13:
+                    break;
+                default:
+                    System.out.println("Please enter a valid choice");
+                    break;
+            }
         }
+    }
+
+    public static void addSong(Scanner console, Connection con) throws SQLException {
+        CallableStatement callsp;
+        //Add Song
+        System.out.print("Song Name: ");
+        console.nextLine(); //Bypass Bug
+        String songTitle = console.nextLine();
+        System.out.print("Song Duration: ");
+        String songDuration = console.nextLine();
+        //Call for the stored procedure in mySQL
+        String callSong = "{call addSong(?,?)}";
+        callsp = con.prepareCall(callSong);
+        //Set Values for Song
+        callsp.setString(1, songTitle);
+        callsp.setString(2, songDuration);
+        callsp.executeUpdate();
+    }
+
+    public static void addArtist(Scanner console, Connection con) throws SQLException {
+        CallableStatement callsp;
+        //Add Artist
+        System.out.print("Artist's Last Name: ");
+        String artistLastName = console.nextLine();
+        System.out.print("Artist's First Name: ");
+        String artistFirstName = console.nextLine();
+        System.out.print("Screen Name: ");
+        String screenName = console.nextLine();
+        System.out.print("Gender: ");
+        String gender = console.nextLine();
+        //Call for the stored procedure
+        String callArtist = "{call addArtist(?,?,?,?)}";
+        callsp = con.prepareCall(callArtist);
+        //Set Values
+        callsp.setString(1, artistLastName);
+        callsp.setString(2, artistFirstName);
+        callsp.setString(3, screenName);
+        callsp.setString(4, gender);
+        callsp.executeUpdate();
     }
     
     public static void menu(){
